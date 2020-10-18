14a6116cc6712cb943b734367f01d17001c1295a
Agunahwan Absin <vair.agun@gmail.com>
2020-10-18 14:39:35 +0700

    Persistent Keypad

--- ButtonLogger/ButtonLogger/ButtonLogger/App.xaml.cs
+++ ButtonLogger/ButtonLogger/ButtonLogger/App.xaml.cs
@@ -6,13 +6,23 @@ namespace ButtonLogger
 {
     public partial class App : Application
     {
+        const string displayLabelText = "displayLabelText";
+
         public App()
         {
+            if (Properties.ContainsKey(displayLabelText))
+            {
+                DisplayLabelText = (string)Properties[displayLabelText];
+            }
+
             //MainPage = new ButtonLoggerPage();
             //MainPage = new TwoButtonsPage();
             //MainPage = new ButtonLambdasPage();
-            MainPage = new SimplestKeypadPage();
+            //MainPage = new SimplestKeypadPage();
+            MainPage = new PersistentKeypadPage();
         }
+
+        public string DisplayLabelText { set; get; }
 
         protected override void OnStart()
         {
@@ -20,6 +30,7 @@ namespace ButtonLogger
 
         protected override void OnSleep()
         {
+            Properties[displayLabelText] = DisplayLabelText;
         }
 
         protected override void OnResume()
